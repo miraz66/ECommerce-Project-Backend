081bb8e3da64d422af926b9a61c7ed27935448c9
miraz66 <mirajulislam0509@gmail.com>
2024-10-31 13:39:02 +0600

    Create Product model

--- database/factories/SalesFactory.php
+++ database/factories/SalesFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,13 @@ class SalesFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::inRandomOrder()->value('id') ?? User::factory(),
+            'product_id' => Product::inRandomOrder()->value('id') ?? Product::factory(),
+            'name' => $this->faker->name(),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'price' => $this->faker->numberBetween(100, 10000),
+            'image' => $this->faker->imageUrl(),
+            'description' => $this->faker->text(),
         ];
     }
 }
